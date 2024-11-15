392c3aeb26eefb9df4f604b792b2d7507481ad1b
Aditya <adityakumar8340203184@gmail.com>
2024-11-15 13:33:29 +0530

    frontend directory changed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middleware to parse JSON and URL-encoded form data
-app.use(express.static(path.join(__dirname, '/restaurant-website')));
+app.use(express.static(path.join(__dirname, '../restaurant-website')));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
